11dd1632c3c6b7bd5c3ed749fa32acba7ea838c1
lihaorong <hoyoung.lee@tenxcloud.com>
2021-06-03 00:18:08 +0800

    controller test

---
+++ src/main/java/com/shop/admin/model/Book.java
@@ -0,0 +1,48 @@
+package com.shop.admin.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="book")
+public class Book {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    private String name;
+    private String author;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    private Float price;
+
+
+}
