eaf1c8c0070d98a1fd2541d72132d855f83ed7a8
lihaorong <hoyoung.lee@tenxcloud.com>
2021-06-04 09:42:08 +0800

    multifile upload

--- src/main/java/com/shop/admin/controller/FileUploadController.java
+++ src/main/java/com/shop/admin/controller/FileUploadController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.UUID;
 
@@ -37,25 +39,26 @@ public class FileUploadController {
 
 
     @PostMapping("/uploads")
-    public String upload(MultipartFile[] uploadFiles, HttpServletRequest req) {
+    public ArrayList<String> upload(MultipartFile[] uploadFiles, HttpServletRequest req) {
         String realPath = req.getSession().getServletContext().getRealPath("/uploadFile/");
         String format = sdf.format(new Date());
         File folder = new File(realPath + format);
         if (!folder.isDirectory()) {
             folder.mkdirs();
         }
+        ArrayList filePaths = new ArrayList<String>();
         for (MultipartFile uploadFile : uploadFiles) {
             String oldName = uploadFile.getOriginalFilename();
             String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."), oldName.length());
             try {
                 uploadFile.transferTo(new File(folder, newName));
                 String filePath = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/uploadFile/" + format + newName;
-                return filePath;
+                filePaths.add(filePath);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        return "上传失败";
+        return filePaths;
     }
 }
