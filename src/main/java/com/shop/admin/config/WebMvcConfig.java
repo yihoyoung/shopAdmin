d6713da10bf51da97229f79dc8052ca307ea9caf
lihaorong <hoyoung.lee@tenxcloud.com>
2021-06-06 21:36:07 +0800

    add cors 请求

--- src/main/java/com/shop/admin/config/WebMvcConfig.java
+++ src/main/java/com/shop/admin/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.shop.admin.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(1800)
+                .allowedOrigins("*");
     }
 }
---
+++ src/main/java/com/shop/admin/model/ErrorAttribute.java
@@ -0,0 +1,17 @@
+package com.shop.admin.model;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+public class ErrorAttribute extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("costomMessage", "出错了!");
+        errorAttributes.remove("error");
+        return errorAttributes;
+    }
+}
